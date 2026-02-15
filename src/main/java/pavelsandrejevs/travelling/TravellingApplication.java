a07a4904210492434820f4e8fe0d92f50235d807
Pavels Andrejevs <pavels.andrejevs@gmail.com>
2026-02-15 17:08:29 +0200

    Time to celebrate!

---
+++ src/main/java/pavelsandrejevs/travelling/TravellingApplication.java
@@ -0,0 +1,13 @@
+package pavelsandrejevs.travelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pavelsandrejevs/travelling/TravellingApplicationTests.java
@@ -0,0 +1,13 @@
+package pavelsandrejevs.travelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
